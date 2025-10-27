5262d378d7164abf9bb3b3db020e7b75e62efbf2
Arsema Yemiru <arsemayemiru610@gmail.com>
2025-10-27 17:30:30 +0300

    Fix failing test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "has a valid factory" do
-  expect(create(:user)).not_to be_valid
+  expect(create(:user)).to be_valid
   end
 
   it "validates name cannot be nil" do
